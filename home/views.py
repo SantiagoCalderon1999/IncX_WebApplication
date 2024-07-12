fc80472a213cf23e84a873228f2ee00cb0f30abf
Santiago Calderón <santiago.calderon@quorumsoftware.com>
2024-07-12 21:25:17 +0100

    Use backend only for computation of explanation

--- home/views.py
+++ home/views.py
@@ -1,47 +1,37 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from .models import *
-from django.core.mail import EmailMessage
-from django.views.decorators import gzip
-from django.http import StreamingHttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import json
+import base64
 import cv2
-import threading
+import numpy as np
 from incrementalexplainer import increx, yolo, d_rise
 
-@gzip.gzip_page
-def Home(request):
-    try:
-        cam = VideoCamera()
-        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
-    except:
-        pass
-    return render(request, 'home.html')
-
-class VideoCamera(object):
+class VideoProcessor:
+    is_frame_processed = False
     model = yolo.Yolo()
-    explainer = d_rise.DRise(model, num_mutants=1000)
+    explainer = d_rise.DRise(model, num_mutants=500)
     incRex = increx.IncRex(model, explainer, saliency_map_divisions=100)
-        
-    def __init__(self):
-        self.video = cv2.VideoCapture(0)
-        (self.grabbed, self.frame) = self.video.read()
-        threading.Thread(target=self.update, args=()).start()
 
-    def __del__(self):
-        self.video.release()
-
-    def get_frame(self):
-        image = self.frame
-        _, img_result = self.incRex.explain_frame(image)
+    @staticmethod
+    def process_frame(frame):
+        # Convert the frame to an OpenCV image
+        nparr = np.frombuffer(base64.b64decode(frame.split(',')[1]), np.uint8)
+        frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        _, img_result = VideoProcessor.incRex.explain_frame(frame)
+        img_result = cv2.cvtColor(img_result, cv2.COLOR_RGB2BGR)
         _, jpeg = cv2.imencode('.jpg', img_result)
         return jpeg.tobytes()
 
-    def update(self):
-        while True:
-            (self.grabbed, self.frame) = self.video.read()
+@csrf_exempt
+def process_frame(request):
+    if request.method == 'POST':
+        frame_data = json.loads(request.body)['frame']
+        processed_frame = VideoProcessor.process_frame(frame_data)
+        response_data = {'processed_frame': base64.b64encode(processed_frame).decode('utf-8')}
+        return JsonResponse(response_data)
+    return JsonResponse({'error': 'Invalid request method'}, status=400)
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+def video_stream(request):
+    return render(request, 'home.html')
